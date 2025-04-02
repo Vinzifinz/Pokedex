84995a6a12143f044f4af4481232be204321af48
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-04-02 15:55:37 +0200

    Component2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,27 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
+
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image pokemon Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc !== undefined ? (
+            <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+            <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
 
 }
 
-export default PokemonCard
+export default PokemonCard;
 
