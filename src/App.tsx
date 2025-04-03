93e83358fc183e7ffa21c6e152f5f90f5ee01ab1
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-04-03 15:37:30 +0200

    rendering-list.1 done

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,21 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
@@ -24,9 +39,12 @@ function App() {
 
   return (
     <div>
+      <nav>
+        {pokemonList.map((pokemon) => (
+          <button key={pokemon.name} onClick={() => setPokemonName(pokemon.name)}>{pokemon.name}</button>
+        ))}
+      </nav>
       <PokemonCard pokemon={pokemon} />
-      <button onClick={() => setPokemonName("bulbasaur")}>Bulbasaur</button>
-      <button onClick={() => setPokemonName("mew")}>Mew</button>
     </div>
   );
 }
